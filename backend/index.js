5bb38a513aee526782e393362a36267a27b0a79c
Majd <majdbitar2003@gmail.com>
2024-08-05 20:41:57 +0300

    Added userModel

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,7 @@ import dbConnection from "./connection.js";
 dotenv.config();
 
 const server = express();
-server.listen(process.env.SERVVER_PORT,()=>{dbConnection()});
+server.listen(process.env.SERVER_PORT,()=>{
+    console.log(`server running on port ${process.env.SERVER_PORT}`);
+    dbConnection();
+});
