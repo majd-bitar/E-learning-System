37aa8edb59f7d6e2999f368de30942c943df9100
Majd <majdbitar2003@gmail.com>
2024-08-05 23:27:55 +0300

    Added withdarawlModel

---
+++ backend/models/withdrawalModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const withdrawalSchema = new mongoose.Schema({
+    user:{type:mongoose.Schema.Types.ObjectId, ref:'User',required:true},
+    class:{type:mongoose.Schema.Types.ObjectId, ref:'Class',required:true},
+    status:{type:String,enum:['pending','approved','rejected'],required:true},
+},{timestamps:true});
+
+const Withdrawal = mongoose.model(withdrawalSchema);
+
+export default Withdrawal;
