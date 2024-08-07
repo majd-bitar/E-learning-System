0831a1dc7a101cb5c60a7f5f2fa7850fd992e04a
Majd <majdbitar2003@gmail.com>
2024-08-07 23:48:05 +0300

    Initialized electron with react

---
+++ admin/src/index.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import {createRoot} from 'react-dom/client';
+import Home from './components/Home.jsx';
+
+const root = createRoot(document.body);
+root.render(
+    <Home></Home>
+)
